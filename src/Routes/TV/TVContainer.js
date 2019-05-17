8210e48b03095370e84f5b0c0b892744c60809ff
gwuiinkim <gwuiinkim@gmail.com>
2019-05-17 19:41:40 +0900

    container presenter part two

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -2,7 +2,25 @@ import React, { Component } from "react";
 import TVPresenter from "./TVPresenter";
 
 export default class extends Component {
+  state = {
+    topRated: null,
+    popular: null,
+    airingToday: null,
+    loading: true,
+    error: null
+  };
   render() {
-    return <TVPresenter />;
+    const { topRated, popular, airingToday, loading, error } = this.state;
+    return (
+      <TVPresenter
+        topRated={topRated}
+        popular={popular}
+        airingToday={airingToday}
+        loading={loading}
+        error={error}
+      />
+    );
   }
 }
+
+// 여러 개 동시에 복사 붙여넣기 할 때 각 component 별로 복사 붙여넣기 가능함.
